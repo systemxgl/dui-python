bc7319af3a1b3c7ce0c3a9ff2d3adbace6cdf722
systemxgl <123218497@qq.com>
2017-10-19 17:23:31 +0800

    添加 打印内容格式 导航链接

--- Demo.py
+++ Demo.py
@@ -12,6 +12,7 @@ result1 = PrintHelper.getServiceState('设备编号')
 #打印内容
 # 返回数据格式 {"TaskId":3949406,"Code":200,"Message":"成功"}
 content = Utils.strToBase64('要打印的内容')
+# 格式详见 https://github.com/systemxgl/dui-api 或 http://www.mstching.com/openapi.pdf
 jsonContent="[{\"Alignment\":0,\"BaseText\":\"" + content + "\",\"Bold\":0,\"FontSize\":0,\"PrintType\":0}]"
 result2 = PrintHelper.printContent('设备编号',jsonContent,0) #0改成用户设备绑定返回的OpenUserId即可
 
